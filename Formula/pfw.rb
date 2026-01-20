adcbfa7f3a91cd85fc6b1fa177823ca735f18581
Stephen Celis <stephen@stephencelis.com>
2026-01-20 09:23:21 -0800

    wip

--- Formula/pfw.rb
+++ Formula/pfw.rb
@@ -1,8 +1,10 @@
 class Pfw < Formula
   desc "The Point-Free Way CLI for installing/updating skill documents"
-  # homepage "https://github.com/pointfreeco/pfw-cli"
+  homepage "https://github.com/pointfreeco/pfw-cli"
   url "https://github.com/pointfreeco/pfw-cli/archive/refs/heads/main.tar.gz"
   version "0.0.1"
+
+  sha256 "201036afc2d1b4a32b42e468f08f740b4ff6ddd616640c16446cc0f91718866d"
 
   def install
     system "swift", "build",
