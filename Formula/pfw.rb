e8249c0603517d54cdb5a16ffa4b773c7fe44713
Stephen Celis <stephen@stephencelis.com>
2026-01-28 11:17:10 -0800

    Bump `pfw` to 0.0.2
    Closes #2.
    
    Signed-off-by: Stephen Celis <me@stephencelis.com>

--- Formula/pfw.rb
+++ Formula/pfw.rb
@@ -1,16 +1,9 @@
 class Pfw < Formula
   desc "CLI for the Point-Free Way"
   homepage "https://github.com/pointfreeco/pfw"
-  url "https://github.com/pointfreeco/pfw/archive/refs/tags/0.0.1.tar.gz"
+  url "https://github.com/pointfreeco/pfw/archive/refs/tags/0.0.2.tar.gz"
 
-  sha256 "0fe2b206bdeec050a942281cb5f6b527f539a2ae7e7b2ae9406620fa639f36e0"
-
-  bottle do
-    root_url "https://github.com/pointfreeco/homebrew-tap/releases/download/pfw-0.0.1"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "6c2f3086a8ba699ad7e5c7dbf6e6b662a7f18c449416f07016fc06b7d93ce152"
-    sha256 cellar: :any_skip_relocation, sequoia:     "db9330129e453264304b9b5094033552481b7e7becf2d48b62408369bb3a6562"
-  end
+  sha256 "855aac98cbce3c983c352e11c2e3d3e0974f30dba6063a647496c6312501fbdb"
 
   depends_on xcode: ["16.0", :build]
 
