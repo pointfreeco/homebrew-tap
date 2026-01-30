070bdde27d655de70bfd58456f76180fe170258e
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2026-01-30 01:56:26 +0000

    pfw: add 0.0.5 bottle.

--- Formula/pfw.rb
+++ Formula/pfw.rb
@@ -5,6 +5,11 @@ class Pfw < Formula
 
   sha256 "cf85c1d5241fb07e68b30796c2119c12b4ee9b3dc82112c7e48b93232a2cdd10"
 
+  bottle do
+    root_url "https://github.com/pointfreeco/homebrew-tap/releases/download/pfw-0.0.5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "eaffed4ad66589e6dad72d676729d582aac1ca347200bce8f7931ba92f12586d"
+  end
+
   depends_on xcode: ["16.0", :build]
 
   uses_from_macos "swift" => :build
